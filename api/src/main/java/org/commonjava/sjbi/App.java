5864eb8ea780441ccb99cf3003b8aedf9c531082
John Casey <jdcasey@commonjava.org>
2010-11-24 16:15:51 -0500

    creating SJBI project skeleton, starting with API.

---
+++ api/src/main/java/org/commonjava/sjbi/App.java
@@ -0,0 +1,13 @@
+package org.commonjava.sjbi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
